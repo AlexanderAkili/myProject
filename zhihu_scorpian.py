6eac5af322235732b7b02407b3e3cfd415d66fd5
AlexanderAkili <125285222+AlexanderAkili@users.noreply.github.com>
2023-03-08 21:47:53 +0800

    Add files via upload

---
+++ zhihu_scorpian.py
@@ -0,0 +1,25 @@
+# code UTF-8
+#  
+# Author: PinkScorpian
+import re
+import urllib.request
+
+def get_html(url):
+	page = urllib.request.urlopen(url)
+	html = page.read()
+	#print(html)
+	return html.decode("utf-8")
+
+def main():
+	url = "https://zhuanlan.zhihu.com/p/33951380"
+	html = get_html(url)
+	r =  r = r"[\u4e00-\u9fa5]+"
+	comment_result = re.findall(r,html)
+	#print(comment_result)
+	print(len(comment_result))
+
+if __name__ == "__main__":
+	main()
+
+
+ 
